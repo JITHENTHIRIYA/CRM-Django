9a715fe12d2832848928885535798db16250b611
JITHENTHIRIYA <69334023+JITHENTHIRIYA@users.noreply.github.com>
2021-08-31 01:16:16 -0700

    Reset Password Email configuration

--- crm/settings.py
+++ crm/settings.py
@@ -129,3 +129,12 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
+
+#SMTP CONFIG
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'g103yatest@gmail.com'
+EMAIL_HOST_PASSWORD = 'g2testsgmail'
