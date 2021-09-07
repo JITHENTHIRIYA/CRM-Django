77b9112c541a5bea7538a234cd00018c35c3cc2f
JITHENTHIRIYA <69334023+JITHENTHIRIYA@users.noreply.github.com>
2021-09-07 06:22:18 +0530

    Migrated to POSTGRES database

--- crm/settings.py
+++ crm/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER' : 'postgres',
+        'PASSWORD': 'jithu01',
+        'HOST' : 'localhost',
+        'PORT' : '5432'
+
     }
 }
 
